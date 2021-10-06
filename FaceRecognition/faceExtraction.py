29510c7265b72b9c296bfed2e8bc477ebd5b726a
Aarush Pal <74675248+aarushpal@users.noreply.github.com>
2021-10-06 11:09:07 +0530

    Added Face Recognition files

---
+++ FaceRecognition/faceExtraction.py
@@ -0,0 +1,59 @@
+import cv2
+import numpy as np
+
+# Loading HAAR face classifier
+face_classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+# Load functions
+def face_extractor(img):
+    # Function detects faces and returns the cropped face
+    # If no face detected, it returns the input image
+    
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY) # Converting image to gray
+    faces = face_classifier.detectMultiScale(gray, 1.05, 5)
+    # Param 1 - The gray scale variable — gray in our case
+    # Param 2 - scaleFactor — Parameter specifying how much the image size is reduced
+                # at each image scale
+    # Param 3 - minNeighbors — Parameter specifying how many neighbors each candidate rectangle should have to retain it. This parameter will affect the quality of the detected faces.
+    
+    if faces is ():
+        return None
+    
+    # Crop all faces found
+    for (x,y,w,h) in faces:
+        x=x-10
+        y=y-10
+        cropped_face = img[y:y+h+50, x:x+w+50]
+
+    return cropped_face
+
+# Initialize Webcam
+cap = cv2.VideoCapture(0)
+count = 0
+
+# Collect 100 samples of your face from webcam input
+while True:
+
+    _, frame = cap.read()
+    if face_extractor(frame) is not None:
+        count += 1
+        face = cv2.resize(face_extractor(frame), (400, 400))
+        # face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
+        # Save file in specified directory with unique name
+        file_name_path = './Images/' + str(count) + '.jpg'
+        cv2.imwrite(file_name_path, face)
+
+        # Put count on images and display live count
+        cv2.putText(face, str(count), (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (255,255,0), 2)
+        cv2.imshow('Face Cropper', face)
+        
+    else:
+        print("Face not found")
+        pass
+
+    if cv2.waitKey(1) == 13 or count == 100: #13 is the Enter Key
+        break
+        
+cap.release()
+cv2.destroyAllWindows()      
+print("Collecting Samples Complete")
